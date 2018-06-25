c180b2336bf572efdd9eeefa504ae020432627d2
langletamelie <amelie.langlet@hotmail.com>
2018-06-25 12:25:08 +0200

    exo1

---
+++ assets/js/js3ex1.js
@@ -0,0 +1,8 @@
+//déclaration de la fonction pour changer l'image et ajouter la bordure
+function changeBorder(){
+document.getElementById('image1').style.border = 'red 3px solid';
+}
+//déclaration de la fonction qui change l'image et enlève la bordure
+function changeBorder2(){
+document.getElementById('image1').style.border = '0';
+}
